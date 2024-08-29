05c4b2fd706a2f1ea0a4f9b9e588dd2a78d7565b
Ethan-png1 <et769620@ucf.edu>
2024-08-29 10:31:35 -0400

    PopUp box Version2
    Added the forms to the pop up box

--- index.js
+++ index.js
@@ -7,9 +7,30 @@ const popUpElm = `<div class="card popUp">
                           Featured
                         </div>
                         <ul class="list-group list-group-flush">
-                          <li class="list-group-item">An item</li>
-                          <li class="list-group-item">A second item</li>
-                          <li class="list-group-item">A third item</li>
+                          <li class="list-group-item">
+                          <form>
+                            <div class="form-group">
+                              <label for="nameIn">Full Name</label>
+                              <input type="Name" class="form-control" id="nameIn" placeholder="Richard Leinecker">
+                            </div>
+                          <form>
+                          </li>
+                          <li class="list-group-item">
+                          <form>
+                            <div class="form-group">
+                              <label for="emailIn">Email</label>
+                              <input type="Email" class="form-control" id="emailIn" placeholder="name@example.com">
+                            </div>
+                          <form>
+                          </li>
+                          <li class="list-group-item">
+                          <form>
+                            <div class="form-group">
+                              <label for="numberIn">PhoneNumber</label>
+                              <input type="phoneNumber" class="form-control" id="phoneIn" placeholder="123-456-7890">
+                            </div>
+                          <form>
+                          </li>
                         </ul>
                       </div>`;
 
