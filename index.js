7cf3bba657bd4ff23e1c32f3101b23c3185fdfcc
rohant20 <rtalagadadeevi@gmail.com>
2024-09-03 16:09:04 -0400

    Home Page PopUp and Redesign

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-const addBtm = document.getElementById("addBtm");
-
-const editBtm = document.getElementById("editBtm");
+const addBtn = document.getElementById("addBtn");
+const editBtn = document.getElementById("editBtn");
 
 const popUpElm = (headerText) =>
-                      `<div class="card popUp">
+  `<div class="card popUp">
                         <div class="card-header">
                         ${headerText}
                         </div>
@@ -14,42 +13,38 @@ const popUpElm = (headerText) =>
                               <label for="nameIn">Full Name</label>
                               <input type="Name" class="form-control" id="nameIn" placeholder="Richard Leinecker">
                             </div>
-                          <form>
                           </li>
                           <li class="list-group-item">
-                          <form>
                             <div class="form-group">
                               <label for="emailIn">Email</label>
                               <input type="Email" class="form-control" id="emailIn" placeholder="name@example.com">
                             </div>
-                          <form>
                           </li>
                           <li class="list-group-item">
-                          <form>
                             <div class="form-group">
                               <label for="numberIn">PhoneNumber</label>
                               <input type="phoneNumber" class="form-control" id="phoneIn" placeholder="123-456-7890">
                             </div>
-                          <form>
+                          </form>
                           </li>
                         </ul>
                       </div>`;
-  
 
 
 
-        
 
-const popupFunc = (e,action) => {
-    e.preventDefault();
-    console.log(popUpElm);
-    const headerText = action === 'add' ? 'Add Contact' : 'Edit Contact';
-    document.getElementById("pop").innerHTML = popUpElm(headerText);
+
+
+const popupFunc = (e, action) => {
+  e.preventDefault();
+  console.log(e.target);
+  const headerText = action === 'add' ? 'Add Contact' : 'Edit Contact';
+  document.getElementById("pop").innerHTML = popUpElm(headerText);
 };
 
 
-addBtm.addEventListener("click", (e) => popupFunc(e,'add'));
-editBtm.addEventListener("click", (e) => popupFunc(e,'edit'));
+addBtn.addEventListener("click", (e) => popupFunc(e, 'add'));
+editBtn.addEventListener("click", (e) => popupFunc(e, 'edit'));
 
 
 
