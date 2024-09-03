8a9c7eb72bc424421abd93a69a531b0f56d2741b
Gabrielhs20 <gabo20hernandez@gmail.com>
2024-09-03 19:54:11 -0400

    Updated main.js
    Still working on the functioning of the functions implemented, but the structure of what is needed is there.

--- js/main.js
+++ js/main.js
@@ -1,18 +1,43 @@
-function login(){
-    const username = document.getElementById("login-username").value;
-    const password = document.getElementById("login-password").value;
+//Constants declaration for userName,password and form
+const userName = document.getElementById("usernameInput");
+const password = document.getElementById("passwordInput");
+const form = document.getElementById("form");
 
-    if(username && password){
-        //Logic to check in the database if the username and the password matches
+//event listener for the form in case of an error
+form.addEventListener('submit', (e)=>{
+    e.preventDefault();
+
+    checkInputs();
+});
+
+//Function to check if inputs are correct or valid
+function checkInputs(){
+    const usernameValue = userName.value.trim();
+    const passwordValue = password.value.trim();
+
+    if(usernameValue == ''){
+        setErrorFor(userName, 'Username cannot be blank');
     }
     else{
-       alert("No account has been found with this username. Please sign-up!");
+        setSuccessFor(userName);
+    }
+    if(passwordValue == ''){
+        setErrorFor(password, 'Password cannot be blank');
+    }else if(passwordValue.length < 8){
+        setErrorFor(password, 'Password must be at least 8 characters');
+    }
+    else{
+        setSuccessFor(password);
     }
 }
 
-function signup(){
-    const username = document.getElementById("signup-username").value;
-    const password = document.getElementById("signup-password").value;
+//setError function to display a message when the input for a particular field is incorrect or invalid
+function setErrorFor(input, message){
+    
+}
+
+function setSuccessFor(){
+    //set the border color to green and display a success message
 }
 
 
