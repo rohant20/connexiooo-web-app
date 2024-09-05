06a8fd5b08f04b89eeff10e23cc63104f5506735
Ethan-png1 <et769620@ucf.edu>
2024-09-05 11:53:23 -0400

    JavaScript Time
    Made a simple add contact function, still working through it. The idea is that when you click submit in the modal will add a contact.

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const addBtm = document.getElementById("addBtm");
 const modTitle = document.getElementById('modal-title');
 const editBtm = document.getElementById("editBtm");
 
+const urlBase = '';
+const extension = 'php';
+
 
 function changeAddTitle(){
   modTitle.textContent = 'Add Contact';
@@ -11,6 +14,36 @@ function changeAddTitle(){
 function changeEditTitle(){
   modTitle.textContent = 'Edit Contact';
   console.log('Edit button clicked');
+}
+
+function addContact(){
+  let newName = document.getElementById("nameIn").value;
+  let newEmail = document.getElementById("emailIn").value;
+  let newPhone = document.getElementById("phoneIn").value;
+
+  let tmp = {name: newName, phone: newPhone, email: newEmail};
+  let jsonPayload = JSON.stringify(tmp);
+
+  let url = urlBase + '/add-contact' + extension;
+  let xhr = new XMLHttpRequest();
+  xhr.open("POST", url, true);
+  xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+
+  try
+    {
+        xhr.onreadystatechange = function() 
+        {
+            if (this.readyState == 4 && this.status == 200) 
+            {
+              console.log("Contact added");
+            }
+        };
+        xhr.send(jsonPayload);
+    }
+    catch(err)
+    {
+      console.log("Error console did not work");
+    }
 }
 
 addBtm.addEventListener("click", changeAddTitle);
