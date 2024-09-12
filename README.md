df6034e8af5ba34b9412550441f37a6d305dea5a
rohant20 <rtalagadadeevi@gmail.com>
2024-09-12 15:54:47 -0400

    Complete API calls
    All API endpoints are called, CRUD operations are called, and functionanality is included.

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 # connexiooo-web-app
 COP4331 Contact Manager Small Project
+http://www.connexiooo.xyz/
 
-Testings
+Developed by Group 5:
+    - Project Manager: Rohan Talagadadeevi
+    - Frontend:  Ethan and Gabriel
+    - Backend/API:  JJ and Diego
+    - Database: Logan
--- js/code.js
+++ js/code.js
@@ -10,21 +10,27 @@ const formName = document.getElementById('nameIn');
 const formEmail = document.getElementById('emailIn');
 const formPhone = document.getElementById('phoneIn');
 const contactContainer = document.getElementById("contactList");
+const searchInput = document.getElementById("searchBar");
+const logOutBtn = document.getElementById("logOut");
 
 const urlBase = 'http://www.connexiooo.xyz/LAMPAPI';
 const extension = 'php';
 
-
+let searchTxt = "";
 let userId = 0;
 let firstName = "";
 let lastName = "";
 
 function readCookie() {
     userId = -1;
+    // let data = document.cookie;
+    // console.log(data);
+    // let breakoff = data.split(";");
+    // console.log(breakoff);
+    // let splits = breakoff[2].split(",");
+    // console.log(splits);
     let data = document.cookie;
-    console.log(data);
     let splits = data.split(",");
-    console.log(splits);
     for (var i = 0; i < splits.length; i++) {
         let thisOne = splits[i].trim();
         let tokens = thisOne.split("=");
@@ -48,6 +54,11 @@ function readCookie() {
     }
 }
 
+function logOut() {
+    document.cookie = "firstName=; lastName;userId=;expires=Thu, 01 Jan 1970 00:00:00 UTC";
+    window.location.reload();
+}
+
 function retriveContacts() {
     let user = {
         userId: userId
@@ -65,16 +76,13 @@ function retriveContacts() {
         xhr.onreadystatechange = function () {
 
             if (this.readyState == 4 && this.status == 200) {
-
                 let jsonArr = JSON.parse(xhr.responseText);
-                console.log(jsonArr.length);
-                console.log(jsonArr);
                 if (jsonArr[0].ID > 0) {
                     for (let i = 0; i < jsonArr.length; i++) {
                         let obj = jsonArr[i];
                         contactContainer.insertAdjacentHTML("beforeend",
                             `<div class="card contactCard">
-                                <div class="card-body">
+                                <div class="card-body contactBody">
                                     <div class="contianer">
                                         <div class="row">
                                             <!-- Contact Card Info -->
@@ -220,8 +228,75 @@ function addContact(e) {
 
 
 
-function searchContact() {
-    console.log('search works');
+function searchContact(e) {
+    e.preventDefault();
+    contactContainer.innerHTML = "";
+
+    let searchObj = {
+        userId: userId,
+        search: searchTxt.trim()
+    }
+    //	var tmp = {login:login,password:hash};
+    let jsonPayload = JSON.stringify(searchObj);
+
+    let url = urlBase + '/searchContact.' + extension;
+
+    let xhr = new XMLHttpRequest();
+    xhr.open("POST", url, true);
+    xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+
+    try {
+        xhr.onreadystatechange = function () {
+
+            if (this.readyState == 4 && this.status == 200) {
+
+                let jsonArr = JSON.parse(xhr.responseText).results;
+                if (jsonArr[0].ID > 0) {
+                    for (let i = 0; i < jsonArr.length; i++) {
+                        let obj = jsonArr[i];
+                        contactContainer.insertAdjacentHTML("beforeend",
+                            `<div class="card contactCard">
+                                <div class="card-body">
+                                    <div class="contianer">
+                                        <div class="row">
+                                            <!-- Contact Card Info -->
+                                            <div class="col-md-10">
+                                                <div class="row">
+                                                    <div class="col-md-5">
+                                                        <h3 class="contactName" id="conName-${obj.ID}">${obj.Name}</h3>
+                                                        <h6 class="infoSubtitle">Contact Name</h6>
+    
+                                                    </div>
+                                                    <div class="col-md-3">
+                                                        <h5 class="contactPhone" id="conPhone-${obj.ID}">${obj.Phone}</h5>
+                                                        <h6 class="infoSubtitle">Phone Number</h6>
+                                                    </div>
+                                                    <div class="col-md-4">
+                                                        <h5 class="contactEmail" id="conEmail-${obj.ID}">${obj.Email}</h5>
+                                                        <h6 class="infoSubtitle">Email Address</h6>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="col-md-2">
+                                                <button class="btn btn-info" type="button" data-bs-toggle="modal" data-bs-target="#popUp"
+                                                    id="editBtm" onclick="changeEditTitle(${obj.ID})">Edit</button>
+                                                <button type="button" class="btn btn-danger" id="delBtm" data-bs-toggle="modal"
+                                                    data-bs-target="#deletMod" onclick="deletePrep(${obj.ID})">Delete</button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>`
+                        );
+                    }
+                }
+            }
+        };
+        xhr.send(jsonPayload);
+    }
+    catch (err) {
+        console.log(err);
+    }
 }
 
 
@@ -253,6 +328,10 @@ function deletePrep(id) {
     delBtn.addEventListener("click", () => deleteContact(id, event));
 }
 
+searchInput.addEventListener("change", () => {
+    searchTxt = searchInput.value;
+});
 
+logOutBtn.addEventListener("click", logOut);
 searchBtm.addEventListener("click", searchContact);
 createConBtn.addEventListener("click", changeAddTitle);
